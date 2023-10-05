5cb6b0c9ef60bf1372e6f38839d28ea16e315deb
mohammad <MOHJAS97_FES@HOTMAIL.COM>
2023-10-05 08:51:08 +0300

    Do all requirement
    Do all requirement

--- blogging/admin.py
+++ blogging/admin.py
@@ -4,5 +4,23 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+from django.contrib import admin
+from .models import Post, Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,]
